b8d3b7d702a15c08901b4fde57e83d0cf37ed761
omaima_khaled <omaimakhaled159@gmail.com>
2021-10-19 10:09:38 +0200

    second

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var quotes=[
+    {author:' Oscar Wilde',content:"Be yourself; everyone else is already taken. "},
+    {author:" Marilyn Monroe",content:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don"},
+    {author:"Albert Einstein",content:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."},
+    {author:'Robert Frost',content:"In three words I can sum up everything I've learned about life: it goes on "},
+    {author:"J.K. Rowling",content:"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."},
+    {author:" Albert Einstein",content:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."},
+    {author:' Oscar Wilde',content:"Be yourself; everyone else is already taken. "},
+    {author:"Marilyn Monroe",content:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don"},
+    {author:"Mae West",content:"You only live once, but if you do it right, once is enough"},
+    {author:' Oscar Wilde',content:"Be yourself; everyone else is already taken. "},
+    {author:" Marilyn Monroe",content:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don"},
+    {author:"Albert Einstein",content:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."},
+    {author:' Mahatma Gandhi',content:"Be the change that you wish to see in the world "},
+    {author:"Marilyn Monroe",content:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don"}  
+
+];
+var Content=document.getElementById("quoteContent")
+var Author=document.getElementById("quoteAuthor")
+function getRandomQuote(){
+    var index= Math.floor(Math.random() * quotes.length)
+    Content.innerHTML=" '"+quotes[index].content+"'"
+    Author.innerHTML= "--"+quotes[index].author
+};
+
+
+
+    
+nameInput=document.getElementById("")
+    
+priceInput=document.getElementById("")
+    
+nameInput=document.getElementById("")
+    
+nameInput=document.getElementById("")
+var product=[]
+
+
